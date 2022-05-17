0530dbb7bba52c8530c7590eda688264a59c2278
zhangyongxin <yongxin.zhang@microfocus.com>
2022-05-17 11:02:57 +0800

    add env prams

--- tests/main.ts
+++ tests/main.ts
@@ -1,13 +1,23 @@
 import {test} from '@playwright/test';
 import {delay, SelectorUtil} from '../lib/SelectorUtil'
 
+const {
+    USER_NAME,
+    PASSWORD,
+} = process.env;
+
 let title = 'auto_extend_python_anywhere'
 test(title, async ({page}) => {
     // login
     await page.goto('https://www.pythonanywhere.com/login/');
     await SelectorUtil.continueWaitFor(page, SelectorUtil.byFullText('h1', 'Log in'));
-    await page.fill('id=id_auth-username', 'zhangyongxin');
-    await page.fill('id=id_auth-password', 'Xiaoxin0223!');
+    let userName = USER_NAME || '';
+    let password = PASSWORD || '';
+    if(!userName || !password){
+        process.exit(0);
+    }
+    await page.fill('id=id_auth-username', userName);
+    await page.fill('id=id_auth-password', password);
     await page.click('id=id_next');
     await page.waitForLoadState('domcontentloaded');
 
